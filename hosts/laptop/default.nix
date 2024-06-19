22fcdb587bf796f6bbec5f9f6a1e36d830877be6
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-06-19 14:59:55 +0200

    Update default.nix

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,7 +1,6 @@
 {
   pkgs,
   config,
-  inputs,
   ...
 }: {
   imports = [
@@ -52,19 +51,5 @@
         cpupower
       ]
       ++ [pkgs.cpupower-gui];
-  };
-
-  wayland.windowManager.hyprland = {
-    extraConfig = "
-      monitor=,preferred,auto,1.9
-
-      xwayland {
-        force_zero_scaling = true
-      }
-
-      # toolkit-specific scale
-      env = GDK_SCALE,2
-      env = XCURSOR_SIZE,32
-    ";
   };
 }
