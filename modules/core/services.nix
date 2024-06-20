22816c479ff298c3d9b98f5c093f232003c4c911
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-06-20 12:09:16 +0200

    Update services.nix

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -5,6 +5,14 @@
     gnome.gnome-keyring.enable = true;
     dbus.enable = true;
     fstrim.enable = true;
+    redshift = {
+      enable = true;
+      brightness = {
+        day = "1";
+        night = "0.7";
+      };
+      temperature.night = 1501;
+    };
   };
   services.logind.extraConfig = ''
     # don’t shutdown when power button is short-pressed
