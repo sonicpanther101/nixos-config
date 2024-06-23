118f6af06d8157adf43d6884ef9818af496ebd85
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-06-23 10:21:52 +0200

    Update bootloader.nix

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -4,5 +4,4 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelModules = [ "snd-seq" "snd-rawmidi" ];
 }
