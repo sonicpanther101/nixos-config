447e4592827508ea86a26f34c3cc21ac9c809488
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-06-24 13:16:01 +0200

    Update default.nix

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -12,5 +12,6 @@
     ++ [ (import ./system.nix) ]
     ++ [ (import ./user.nix) ]
     ++ [ (import ./wayland.nix) ]
+    ++ [ (import ./openrgb.nix) ];
     ++ [ (import ./virtualization.nix) ];
 }
