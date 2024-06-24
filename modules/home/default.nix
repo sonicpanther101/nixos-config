902094e973d615d3d15aef52906d05ed99acf799
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-06-24 13:02:28 +0200

    Update default.nix

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -24,5 +24,6 @@
     ++ [(import ./vscodium.nix)]                  # vscode forck
     ++ [(import ./waybar)]                        # status bar
     ++ [(import ./wofi.nix)]                      # launcher
-    ++ [(import ./zsh.nix)];                      # shell
+    ++ [(import ./zsh.nix)]                       # shell
+    ++ [(import ./openrgb.nix)];                  # openrgb
 }
