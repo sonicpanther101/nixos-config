68904858e8568f2494165d5551335fd00fcbac31
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-06-24 12:56:47 +0200

    Update services.nix

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -10,13 +10,39 @@
     # don’t shutdown when power button is short-pressed
     HandlePowerKey=ignore
   '';
-  services.ollama = {
-    enable = true;
-    acceleration = "cuda";
+  services.hardware.openrgb.enable = true;
+  #services.ollama = {
+  #  enable = true;
+  #  acceleration = "cuda";
+  #};
+  #services = {
+  #  udev.packages = with pkgs; [ 
+  #    openrgb-with-all-plugins
+  #  ];
   };
-  services = {
-    udev.packages = with pkgs; [ 
-      openrgb-with-all-plugins
-    ];
+};
+let
+  no-rgb = pkgs.writeScriptBin "no-rgb" ''
+    #!/bin/sh
+    NUM_DEVICES=$(${pkgs.openrgb}/bin/openrgb --noautoconnect --list-devices | grep -E '^[0-9]+: ' | wc -l)
+
+    for i in $(seq 0 $(($NUM_DEVICES - 1))); do
+      ${pkgs.openrgb}/bin/openrgb --noautoconnect --device $i --mode static --color 000000
+    done
+  '';
+in {
+  config = {
+    services.udev.packages = [ pkgs.openrgb ];
+    boot.kernelModules = [ "i2c-dev" ];
+    hardware.i2c.enable = true;
+
+    systemd.services.no-rgb = {
+      description = "no-rgb";
+      serviceConfig = {
+        ExecStart = "${no-rgb}/bin/no-rgb";
+        Type = "oneshot";
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
   };
 }
