84eee34e445f4b577a6892aca50aade2a856b625
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-06-25 11:22:57 +0200

    Update services.nix

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -10,14 +10,17 @@
     # don’t shutdown when power button is short-pressed
     HandlePowerKey=ignore
   '';
+
   services.hardware.openrgb.enable = true;
-  #services.ollama = {
-  #  enable = true;
-  #  acceleration = "cuda";
-  #};
   #services = {
   #  udev.packages = with pkgs; [ 
   #    openrgb-with-all-plugins
   #  ];
   #};
+
+  services.ollama = {
+    enable = true;
+    acceleration = "cuda";
+  };
+  services.nextjs-ollama-llm-ui.enable = true;
 }
