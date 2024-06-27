88bbd0630ea4fe66710eef15fba835d7ade6e792
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-06-27 10:17:40 +0200

    Update virtualization.nix

--- modules/core/virtualization.nix
+++ modules/core/virtualization.nix
@@ -27,4 +27,20 @@
     spiceUSBRedirection.enable = true;
   };
   services.spice-vdagentd.enable = true;
+
+  virtualisation = {
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      #defaultNetwork.settings.dns_enabled = true;
+    };
+ 
+    oci-containers = {
+      backend = "podman";
+ 
+      containers = {
+        open-webui = import ./containers/open-webui.nix;
+      };
+    };
+  };
 }
