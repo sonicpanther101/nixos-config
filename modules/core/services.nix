9804075861d0a4d1352cd0e589a6ad1f1458d811
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-06-27 10:06:02 +0200

    Update services.nix

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -17,4 +17,16 @@
   #    openrgb-with-all-plugins
   #  ];
   #};
+
+  services.ollama = {
+    #package = pkgs.unstable.ollama; # Uncomment if you want to use the unstable channel, see https://fictionbecomesfact.com/nixos-unstable-channel
+    enable = true;
+    acceleration = "cuda"; # Or "rocm"
+    #environmentVariables = { # I haven't been able to get this to work myself yet, but I'm sharing it for the sake of completeness
+      # HOME = "/home/ollama";
+      # OLLAMA_MODELS = "/home/ollama/models";
+      # OLLAMA_HOST = "0.0.0.0:11434"; # Make Ollama accesible outside of localhost
+      # OLLAMA_ORIGINS = "http://localhost:8080,http://192.168.0.10:*"; # Allow access, otherwise Ollama returns 403 forbidden due to CORS
+    #};
+  };
 }
