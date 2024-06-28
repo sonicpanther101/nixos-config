16e544e10f917ce80af203b741317ff49c945f4a
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-06-28 09:11:45 +0200

    Update default.desktop.nix

--- modules/home/default.desktop.nix
+++ modules/home/default.desktop.nix
@@ -1,7 +1,6 @@
 {inputs, username, host, ...}: {
   imports =
        [(import ./default.nix)]
-    ++ [(import ./rider.nix)]                     # C# JetBrain editor
     # ++ [ (import ./steam.nix) ]
     ++ [(import ./unity.nix)];
 }
