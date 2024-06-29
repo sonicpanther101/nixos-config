a935e375dbe10d23e1d2fadcf2b7689db86dc7cf
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-06-29 09:41:30 +0200

    Update openrgb.nix

--- modules/core/containers/openrgb.nix
+++ modules/core/containers/openrgb.nix
@@ -4,4 +4,13 @@
   environment = {
     "TZ" = "Pacific/Auckland";
   };
+
+  extraOptions = [
+  "--rm"
+  "--privileged"
+  "-v /tmp/.X11-unix:/tmp/.X11-unix"
+  "-v /config:/config"
+  "-e DISPLAY=unix$DISPLAY"
+  "-p 6742:6742"
+  ];
 }
