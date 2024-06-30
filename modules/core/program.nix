807ba3793c9b628762779390d83acc89c57d79b2
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-06-30 10:32:48 +0200

    Update program.nix

--- modules/core/program.nix
+++ modules/core/program.nix
@@ -7,10 +7,4 @@
     enableSSHSupport = true;
     # pinentryFlavor = "";
   };
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  };
 }
