2789348f9bfb6ca52ffbf9564f48cf883eb117ab
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-07-01 03:35:55 +0200

    Update bootloader.nix

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -3,7 +3,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.efiSupport = true;
 
