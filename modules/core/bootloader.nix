9f82f0a1c8271b90f646bd539f9ae2b15d67a92b
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-07-01 03:26:25 +0200

    Update bootloader.nix

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -1,9 +1,11 @@
 { inputs, config, pkgs, lib, ... }:
 {
-  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
 
   boot.loader.grub = { ... };
   boot.loader.grub2-theme = {
