5ee74b3fcef1a5e2832036bf0e9247f2aba24954
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-07-01 10:23:34 +0200

    Update packages.nix

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -77,12 +77,28 @@
     unigine-valley
     geekbench
 
-    git gitRepo gnupg autoconf curl
-    procps util-linux m4 gperf unzip
-    cudatoolkit linuxPackages.nvidia_x11
+    git # The program instantly crashes if git is not present, even if everything is already downloaded
+    python310
+    stdenv.cc.cc.lib
+    stdenv.cc
+    ncurses5
+    binutils
+    gitRepo gnupg autoconf curl
+    procps gnumake util-linux m4 gperf unzip
     libGLU libGL
-    xorg.libXi xorg.libXmu freeglut
-    xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib 
-    ncurses5 stdenv.cc binutils
+    glib
+
+    cudatoolkit
+    linuxPackages.nvidia_x11
+    xorg.libXi
+    xorg.libXmu
+    freeglut
+    xorg.libXext
+    xorg.libX11
+    xorg.libXv
+    xorg.libXrandr
+    zlib
+    # for xformers
+    gcc
   ]);
 }
