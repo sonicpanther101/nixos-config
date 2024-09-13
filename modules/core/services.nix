e28b2c2d424c1b5e76af0b5176e20ed4de5354fb
sonicpanther101 <sonicpanther101@gmail.com>
2024-09-13 23:51:17 +1200

    Rebuilt laptop with new flake version 336 current  2024-09-13 23:51:17  24.11.20240910.1355a0c  6.6.50                          *. Updated files:

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -22,4 +22,12 @@
   services.udev.packages = [
     pkgs.iptsd
   ];
+
+  environment.sessionVariables = {
+    # If your cursor becomes invisible
+    WLR_NO_HARDWARE_CURSORS = "1";
+    # Hint electron apps to use wayland
+    NIXOS_OZONE_WL = "1";
+  };
+  
 }
