f09e27d8730a83a014a5d9ccbf42c22d03a4c5fd
sonicpanther101 <sonicpanther101@gmail.com>
2024-10-12 14:21:58 +1300

    Rebuilt desktop with new flake version 541 current  2024-10-12 14:21:58  24.11.20241009.5633bcf  6.6.54                          *. Updated files: flake.lock modules/core/pipewire_pc.nix

--- modules/core/pipewire_pc.nix
+++ modules/core/pipewire_pc.nix
@@ -11,4 +11,22 @@
   environment.systemPackages = with pkgs; [
     pulseaudioFull
   ];
+
+  services.jack = {
+    jackd.enable = true;
+    # support ALSA only programs via ALSA JACK PCM plugin
+    alsa.enable = true;
+    # support ALSA only programs via loopback device (supports programs like Steam)
+    loopback = {
+      enable = false;
+      # buffering parameters for dmix device to work with ALSA only semi-professional sound programs
+      #dmixConfig = ''
+      #  period_size 2048
+      #'';
+    };
+  };
+
+  users.extraUsers.${username}.extraGroups = [ "jackaudio" ];
+
+  boot.kernelModules = [ "snd-seq" "snd-rawmidi" ];
 }
