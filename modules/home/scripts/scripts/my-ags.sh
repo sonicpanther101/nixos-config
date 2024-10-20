54621426bb975f5cdca288bcd0e81dd55c95eeec
sonicpanther101 <sonicpanther101@gmail.com>
2024-10-20 20:12:35 +1300

    Rebuilt desktop with new flake version 545 current  2024-10-20 20:12:35  24.11.20241018.4c2fcb0  6.6.57                          *. Updated files: flake-desktop.lock flake.lock

--- modules/home/scripts/scripts/my-ags.sh
+++ modules/home/scripts/scripts/my-ags.sh
@@ -1,7 +1,7 @@
 ags -q
 hyprctl dispatch exec "[workspace 9 silent] kitty --hold sh -c 'ags -c ~/nixos-config/modules/home/ags/ags/config.js'"
 if playerctl status | grep "Playing"; then
-    sleep 3
+    sleep 5
     playerctl pause
     sleep 0.1
     playerctl play
