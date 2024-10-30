5e12cdae3d66a1d37c4cf52da3d6cfc6253b6881
sonicpanther101 <sonicpanther101@gmail.com>
2024-10-30 16:57:25 +1300

    Rebuilt laptop with new flake version 364 current  2024-10-30 16:57:24  24.11.20240818.8a33541  6.6.46                          *. Updated files: flake.lock

--- modules/home/end_4/hyprland.nix
+++ modules/home/end_4/hyprland.nix
@@ -253,7 +253,7 @@ in
           "Super+Alt, mouse_up, movetoworkspace, +1"
           "Super, F, fullscreen, 0"
           "Super, D, fullscreen, 1"
-          "Super_Alt, F, fakefullscreen, 0"
+          # "Super_Alt, F, fakefullscreen, 0"
           "Super, 1, workspace, 1"
           "Super, 2, workspace, 2"
           "Super, 3, workspace, 3"
@@ -362,9 +362,9 @@ in
         "noanim, sideright"
         "noanim, sideleft"
       ];
-      source = [
-        "./colors.conf"
-      ];
+      # source = [
+      #   "./colors.conf"
+      # ];
     };
   };
 }
