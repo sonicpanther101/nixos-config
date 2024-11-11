feb68e6b7b522e4e5c510f7c106ec1508da71cdc
sonicpanther101 <sonicpanther101@gmail.com>
2024-11-11 14:19:25 +1300

    Rebuilt desktop with new flake version 583 current  2024-11-11 14:19:24  24.11.20241109.76612b1  6.6.60                          *. Updated files: flake.lock

--- modules/home/hyprland/hyprland.nix
+++ modules/home/hyprland/hyprland.nix
@@ -22,4 +22,13 @@
     };
     systemd.enable = true;
   };
+
+  xdg.desktopEntries."org.gnome.Settings" = {
+    name = "Settings";
+    comment = "Gnome Control Center";
+    icon = "org.gnome.Settings";
+    exec = "env XDG_CURRENT_DESKTOP=gnome ${pkgs.gnome-control-center}/bin/gnome-control-center";
+    categories = [ "X-Preferences" ];
+    terminal = false;
+  };
 }
