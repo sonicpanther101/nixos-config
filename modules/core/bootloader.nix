f0c913437e0ad9c54c682cc18508b9a10a07cdae
sonicpanther101 <sonicpanther101@gmail.com>
2024-11-20 20:50:25 +1300

    Rebuilt desktop with new flake version 653 current  2024-11-20 20:50:22  24.11.20241111.dc460ec  6.6.60                          *. Updated files: flake.lock

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -1,7 +1,7 @@
 { inputs, config, pkgs, lib, host, ... }:
 {
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.efiSupport = true;
