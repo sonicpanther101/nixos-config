c68061cfc33831bf4a66749ef4496292fead2ef3
sonicpanther101 <sonicpanther101@gmail.com>
2024-11-23 16:57:47 +1300

    Rebuilt desktop with new flake version 659 current  2024-11-23 16:57:45  24.11.20241111.dc460ec  6.6.60                          *. Updated files: flake.lock modules/home/ags/default.nix

--- modules/home/ags/default.nix
+++ modules/home/ags/default.nix
@@ -13,8 +13,8 @@
     configDir = null; # if ags dir is managed by home-manager, it'll end up being read-only. not too cool.
     # configDir = "./.config/ags";
 
-    extraPackages = with inputs.ags.packages.${pkgs.system}; [
-      # astal
+    extraPackages = with inputs.astal.packages.${pkgs.system}; [
+      astal3
       io
 
       apps
