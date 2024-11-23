034285000f760f5d4f7ba49ae92fdaec7901e2ef
sonicpanther101 <sonicpanther101@gmail.com>
2024-11-23 16:55:30 +1300

    Rebuilt desktop with new flake version 658 current  2024-11-23 16:55:28  24.11.20241111.dc460ec  6.6.60                          *. Updated files: flake.lock

--- modules/home/ags/default.nix
+++ modules/home/ags/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     inputs.ags.homeManagerModules.default
+
   ];
 
   home.packages = with pkgs; [
@@ -12,21 +13,24 @@
     configDir = null; # if ags dir is managed by home-manager, it'll end up being read-only. not too cool.
     # configDir = "./.config/ags";
 
-    extraPackages = with pkgs; [
-      inputs.ags.packages.${pkgs.system}.apps
-      inputs.ags.packages.${pkgs.system}.auth
-      inputs.ags.packages.${pkgs.system}.battery
-      inputs.ags.packages.${pkgs.system}.bluetooth
-      # inputs.ags.packages.${pkgs.system}.cava
-      inputs.ags.packages.${pkgs.system}.greet
-      inputs.ags.packages.${pkgs.system}.hyprland
-      inputs.ags.packages.${pkgs.system}.mpris
-      inputs.ags.packages.${pkgs.system}.network
-      inputs.ags.packages.${pkgs.system}.notifd
-      inputs.ags.packages.${pkgs.system}.powerprofiles
-      inputs.ags.packages.${pkgs.system}.river
-      inputs.ags.packages.${pkgs.system}.tray
-      inputs.ags.packages.${pkgs.system}.wireplumber
+    extraPackages = with inputs.ags.packages.${pkgs.system}; [
+      # astal
+      io
+
+      apps
+      auth
+      battery
+      bluetooth
+      cava
+      greet
+      hyprland
+      mpris
+      network
+      notifd
+      powerprofiles
+      river
+      tray
+      wireplumber
     ];
   };
 }
