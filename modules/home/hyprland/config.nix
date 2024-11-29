be0bbc4e6238615fc4b1f2bbd44ef4394d5ab3a5
sonicpanther101 <sonicpanther101@gmail.com>
2024-11-29 20:12:52 +1300

    Rebuilt desktop with new flake version 667 current  2024-11-29 20:12:50  25.05.20241125.4633a7c  6.6.63                          *. Updated files: flake.lock

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -17,6 +17,12 @@
         workspace_swipe_create_new = true;
       };
 
+      plugin = {
+        hyprsplit = {
+          num_workspaces = 6;
+        };
+      };
+
       # autostart
       exec-once = 
         if (host == "desktop") then [
@@ -223,6 +229,23 @@
         "$mainMod, right, movefocus, r"
         "$mainMod, up, movefocus, u"
         "$mainMod, down, movefocus, d"
+
+        # hyprsplit
+        "bind = SUPER, M, split:grabroguewindows"
+
+        "bind = SUPER, 1, split:workspace, 1"
+        "bind = SUPER, 2, split:workspace, 2"
+        "bind = SUPER, 3, split:workspace, 3"
+        "bind = SUPER, 4, split:workspace, 4"
+        "bind = SUPER, 5, split:workspace, 5"
+        "bind = SUPER, 6, split:workspace, 6"
+
+        "bind = SUPER SHIFT, 1, split:movetoworkspacesilent, 1"
+        "bind = SUPER SHIFT, 2, split:movetoworkspacesilent, 2"
+        "bind = SUPER SHIFT, 3, split:movetoworkspacesilent, 3"
+        "bind = SUPER SHIFT, 4, split:movetoworkspacesilent, 4"
+        "bind = SUPER SHIFT, 5, split:movetoworkspacesilent, 5"
+        "bind = SUPER SHIFT, 6, split:movetoworkspacesilent, 6"
 
         # switch workspace
         "$mainMod, 1, workspace, 1"
