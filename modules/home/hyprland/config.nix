95bbc27305fd29cf6478a06166d9a2441d30714a
sonicpanther101 <sonicpanther101@gmail.com>
2024-12-04 16:36:21 +1300

    Rebuilt desktop with new flake version 687 current  2024-12-04 16:36:20  25.05.20241125.4633a7c  6.6.63                          *. Updated files: flake.lock

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -380,8 +380,8 @@
       ];
 
       monitor = if (host == "laptop") then [",preferred,auto,1.9"] else [
-        "DP-1,preferred,auto,1.083333"
-        "HDMI-A-1,preferred,auto,1"
+        "DP-1,2560x1440@170,auto,1.066667"
+        "HDMI-A-1,1920x1080@100,auto,1"
         ];
 
       xwayland.force_zero_scaling = true;
