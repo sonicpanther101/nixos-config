a1cd8214e39113de8d445a43755406c3092e8722
sonicpanther101 <sonicpanther101@gmail.com>
2024-12-04 16:18:52 +1300

    Rebuilt desktop with new flake version 680 current  2024-12-04 16:18:51  25.05.20241125.4633a7c  6.6.63                          *. Updated files: flake.lock

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -380,7 +380,7 @@
       ];
 
       monitor = if (host == "laptop") then [",preferred,auto,1.9"] else [
-        "DP-1,preferred,auto,1.6"
+        "DP-1,preferred,auto,1.4"
         "HDMI-A-1,preferred,auto,0.8"
         ];
 
