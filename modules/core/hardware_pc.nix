24aec14d0f4931086196f60f26753bf8dcb2e4e9
sonicpanther101 <sonicpanther101@gmail.com>
2024-12-08 15:03:19 +1300

    Rebuilt desktop with new flake version 712 current  2024-12-08 15:03:17  25.05.20241203.55d15ad  6.6.63                          *. Updated files:

--- modules/core/hardware_pc.nix
+++ modules/core/hardware_pc.nix
@@ -26,13 +26,11 @@
 
   services.openssh = {
     enable = true;
-    ports = [ 22 ];
+    ports = [22];
     settings = {
       PasswordAuthentication = true;
-      AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
-      UseDns = true;
-      X11Forwarding = false;
-      PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+      AllowUsers = null;
+      PermitRootLogin = "yes";
     };
   };
 
