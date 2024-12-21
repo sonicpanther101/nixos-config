e70ea65b6324c3418231333b2bf9ff2fd7fd90a0
sonicpanther101 <sonicpanther101@gmail.com>
2024-12-22 01:03:05 +1300

    Rebuilt laptop with new flake version 19 current  2024-12-22 01:03:03  25.05.20241203.55d15ad   6.6.63                          *. Updated files:

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -105,7 +105,7 @@
       };
 
       dwindle = {
-        # no_gaps_when_only = true;
+        no_gaps_when_only = true;
         force_split = 0;
         special_scale_factor = 1.0;
         split_width_multiplier = 1.0;
@@ -117,7 +117,7 @@
 
       master = {
         special_scale_factor = 1;
-        # no_gaps_when_only = false;
+        no_gaps_when_only = false;
       };
 
       decoration = {
