eb01f4e41f5da4173782ff756392ebbc0668ac49
sonicpanther101 <sonicpanther101@gmail.com>
2025-01-02 21:05:51 +1300

    nested tasks done functionally

--- modules/home/ags/new ags/widget/components/dashboard/tasks.tsx
+++ modules/home/ags/new ags/widget/components/dashboard/tasks.tsx
@@ -3,8 +3,13 @@ import { Gtk, Widget } from "astal/gtk3";
 import Pango from "gi://Pango?version=1.0";
 
 const task_list = Variable<TaskDefinition[]>([])
+const changed = Variable<boolean>(false)
 let gistID = ""
 let token = ""
+
+function changedHandler() {
+  changed.set(!changed.get())
+}
 
 readFileAsync(`/home/adam/.cache/astal/secrets.json`).then((secrets) => {
   let parsed = JSON.parse(secrets)
@@ -58,35 +63,86 @@ interface TaskDefinition {
   subtasksShown?: boolean;
 }
 
-const createTasks = (definition: TaskDefinition, i: number, treePosition: number[]) => (
+function getTarget(treePosition: number[], handler: (target: TaskDefinition) => void) {
+  let temp = task_list.get();
+  const target = treePosition.slice(1).reduce((acc, curr, index) => {
+    if (index >= treePosition.length - 1) {
+      return acc;
+    } else {
+      return acc.subtasks?.[curr] || acc; 
+    }
+  }, temp[treePosition[0]]); 
+
+  handler(target);
+  task_list.set(temp); 
+  changedHandler();
+}
+
+function getTargetParent(treePosition: number[], handler: (parent: TaskDefinition) => void) {
+  let temp = task_list.get();
+  const parent = treePosition.slice(1).reduce((acc, curr, index) => {
+    if (index >= treePosition.length - 2) {
+      return acc;
+    } else {
+      return acc.subtasks?.[curr] || acc; 
+    }
+  }, temp[treePosition[0]]); 
+
+  handler(parent);
+  task_list.set(temp); 
+  changedHandler();
+}
+
+function createTasks(definition: TaskDefinition, i: number, treePosition: number[]) {
+  
+  return (
   <box
     hexpand
     className={i % 2 === 0 ? "even-scroll" : "odd-scroll"}
   >
     <box
       vertical
+      vexpand={false}
     >
       <button
+        vexpand
         className={`task-up ${i === 0 ? "task-up-disabled" : ""}`}
         onClick={() => {
           let temp = definition
-          let templist = task_list.get()
-          templist.splice(i, 1)
-          templist.splice(i - 1, 0, temp)
-          task_list.set(templist)
+          if (treePosition.length === 1) {
+            let templist = task_list.get()
+            templist.splice(i, 1)
+            templist.splice(i - 1, 0, temp)
+            task_list.set(templist)
+            changedHandler()
+          } else {
+            getTargetParent(treePosition, (parent) => {
+              parent.subtasks?.splice(i, 1)
+              parent.subtasks?.splice(i - 1, 0, temp)
+            })
+          }
           writeUpdate()
         }}
       >
         <label label="⏶" className="task-button-label" />
       </button>
       <button
+        vexpand
         className={`task-down ${i === task_list.length - 1 ? "task-down-disabled" : ""}`}
         onClick={() => {
           let temp = definition
-          let templist = task_list.get()
-          templist.splice(i, 1)
-          templist.splice(i + 1, 0, temp)
-          task_list.set(templist)
+          if (treePosition.length === 1) {
+            let templist = task_list.get()
+            templist.splice(i, 1)
+            templist.splice(i + 1, 0, temp)
+            task_list.set(templist)
+            changedHandler()
+          } else {
+            getTargetParent(treePosition, (parent) => {
+              parent.subtasks?.splice(i, 1)
+              parent.subtasks?.splice(i + 1, 0, temp)
+            })
+          }
           writeUpdate()
         }}
       >
@@ -96,10 +152,9 @@ const createTasks = (definition: TaskDefinition, i: number, treePosition: number
     <button
       className="task-edit"
       onClick={() => {
-        input.set(definition.label)
-        Entry.grab_focus()
-        task_list.set(task_list.get().splice(i, 1))
-        writeUpdate()
+        getTarget(treePosition, (target) => {
+          target.editMode = !target.editMode
+        })
       }}
     >
       <label label="✎" className="task-button-label" />
@@ -107,32 +162,56 @@ const createTasks = (definition: TaskDefinition, i: number, treePosition: number
     {definition.subtasks && <button
       className="toggle-subtasks"
       onClick={() => {
-        let temp = task_list.get();
-        if (treePosition.length === 1) {
-          temp[treePosition[0]].subtasksShown = !temp[treePosition[0]].subtasksShown;
-          // print(temp[treePosition[0]].subtasksShown)
-        }
-        task_list.set(temp);
-        // print(JSON.stringify(task_list.get()))
+        getTarget(treePosition, (target) => {
+          target.subtasksShown = !target.subtasksShown
+        })
       }}
     >
-      <label label={JSON.stringify(definition)} css="font-size: 1rem;" wrap wrapMode={Pango.WrapMode.WORD_CHAR} className="task-button-label" />
+      <label label={definition.subtasksShown ? "▼" : "▶"} css="font-size: 1rem;"className="task-button-label" />
     </button>}
     <box vertical>
-      <label hexpand label={definition.label} wrap wrapMode={Pango.WrapMode.WORD_CHAR} halign={Gtk.Align.START} className="task" />
+      {definition.editMode ?
+        <entry text={definition.label} className="task" onActivate={(self) => getTarget(treePosition, (target) => { target.editMode = false; target.label = self.get_text() })} hexpand halign={Gtk.Align.START} /> :
+        <label hexpand label={definition.label} wrap wrapMode={Pango.WrapMode.WORD_CHAR} halign={Gtk.Align.START} className="task" />}
       {definition.subtasksShown && definition.subtasks && definition.subtasks.map((task, i) => createTasks(task, i, [...treePosition, i]))}
     </box>
     <button
+      className="subtask-add"
+      onClick={() => {
+        getTarget(treePosition, (target) => {
+          target.subtasks = target.subtasks || []
+          target.subtasks.splice(0, 0, { label: "", editMode: true })
+          target.subtasksShown = true
+        })
+      }}
+    >
+      <label label="+" className="task-button-label" />
+    </button>
+    <button
       className="task-delete"
       onClick={() => {
-        let temp = task_list.get()
-        temp.splice(i, 1)
-        task_list.set(temp)
+        if (treePosition.length === 1) {
+          let temp = task_list.get()
+          temp.splice(i, 1)
+          task_list.set(temp)
+          changedHandler()
+        } else {
+          getTargetParent(treePosition, (parent) => {
+            parent.subtasks?.splice(i, 1)
+          })
+        }
         writeUpdate()
       }}
-    ><label label="✕" className="task-button-label" /></button>
+      >
+        <label label="✕" className="task-button-label" />
+      </button>
   </box>
-);
+  )
+};
+
+const items = Variable.derive([task_list, changed], (task_list, changed) => {
+  return task_list.map((task, i) => createTasks(task, i, [i]))
+})
  
 function readUpdate() {
   execAsync(['bash', '-c', `curl -L \
@@ -144,15 +223,78 @@ function readUpdate() {
     const data = JSON.parse(out)["files"]["todo.txt"]["content"]
 
     const tasks = JSON.parse(data)
+
+    const complexTasks: TaskDefinition[] = [
+  {
+    label: "Project A",
+    subtasks: [
+      {
+        label: "Phase 1",
+        subtasks: [
+          {
+            label: "Task 1.1",
+            subtasks: [
+              {
+                label: "Subtask 1.1.1",
+              },
+              {
+                label: "Subtask 1.1.2",
+                subtasks: [
+                  {
+                    label: "Subtask 1.1.2.1",
+                  },
+                  {
+                    label: "Subtask 1.1.2.2",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            label: "Task 1.2",
+          },
+        ],
+      },
+      {
+        label: "Phase 2",
+      },
+    ],
+  },
+  {
+    label: "Project B",
+    subtasks: [
+      {
+        label: "Task 2.1",
+        subtasks: [
+          {
+            label: "Subtask 2.1.1",
+          },
+        ],
+      },
+    ],
+  },
+    ];
     
-    task_list.set(tasks)
+    
+    
+    task_list.set(complexTasks)
   }).catch(print)
 }
 
 function writeUpdate() {
   print("update")
 
-  const out = JSON.stringify(task_list.get()).replace(/{/g, '\n{')
+  function removeDisplayProperties(task: TaskDefinition): TaskDefinition {
+    return {
+      label: task.label,
+      subtasks:
+        task.subtasks?.map(subtask =>
+          removeDisplayProperties(subtask)
+        ),
+    };
+  }
+
+  const out = JSON.stringify(task_list.get().map(removeDisplayProperties)).replace(/{/g, '\n{')
 
   print(out)
 
@@ -162,7 +304,8 @@ function writeUpdate() {
     -H "Authorization: Bearer ${token}" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     https://api.github.com/gists/${gistID} \
-    -d '{"description":"Updated Gist","files":{"todo.txt":{"content":"${out}"}}}'`)
+    -d '{"description":"Updated Gist","files":{"todo.txt":{"content":"${out}"}}}'
+  `)
 
   // execAsync(['bash', '-c', `curl -L \
   //   -X PATCH \
@@ -185,10 +328,7 @@ const Tasks = (
       <box
         vertical
       >
-        {bind(task_list).as((tasks) => tasks.map((task, i) => {
-          print(JSON.stringify(task))
-          return createTasks(task, i, [i])
-        }))}
+        {bind(items)}
       </box>
     </scrollable>
     <box>
