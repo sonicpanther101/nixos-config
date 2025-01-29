8f4f4de59524f694bc1a54666a3cd3d48fc8b185
sonicpanther101 <sonicpanther101@gmail.com>
2025-01-29 22:43:45 +1300

    stop mouse and keyboard from waking pc Rebuilt desktop with new flake version 736 current  2025-01-29 22:43:45  25.05.20250126.852ff1d  6.12.11                          *. Updated files:

--- modules/core/services_pc.nix
+++ modules/core/services_pc.nix
@@ -28,7 +28,8 @@
 
   services.udev.extraRules = ''
     # Your rule goes here
-    # SUBSYSTEMS=="usb|hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0361", TAG+="uaccess", TAG+="V6"
+    ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="00c5", ATTR{power/wakeup}="disabled", ATTR{driver/3-2/power/wakeup}="disabled"
+    ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0361", ATTR{power/wakeup}="disabled", ATTR{driver/3-4/power/wakeup}="disabled"
   '';
 
   hardware.xpadneo.enable = true;
