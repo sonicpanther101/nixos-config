5ec529939a9f9de41086d824b4f756ddaa5edf4a
Astro <116870775+sonicpanther101@users.noreply.github.com>
2025-02-11 23:07:12 +1300

    Add arguments

--- initialise.sh
+++ initialise.sh
@@ -8,4 +8,27 @@
 mkdir -p ~/Music
 mkdir -p ~/Pictures/wallpapers
 
-cp /etc/nixos/hardware-configuration.nix hosts/${HOST}/hardware-configuration.nix
+while getopts "anhH:m:" option; do  # anhH
+    case $option in
+        m)
+            message="$OPTARG"
+            ;;
+        H)
+            echo "host: $OPTARG"
+            case $OPTARG in
+                l) 
+                    host="laptop"
+                    h="l";;
+                d) 
+                    host="desktop"
+                    h="d";;
+            esac;;
+        \?) # Invalid option
+            echo "Error: Invalid option"
+            exit;;
+    esac
+done
+
+cp /etc/nixos/hardware-configuration.nix hosts/${host}/hardware-configuration.nix
+
+./modules/home/scripts/scripts/my-install.sh -n -H ${h} -m "${message}"
