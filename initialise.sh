753358a8796a1d4bb0f17d8d6ba0299625a5cae6
Astro <116870775+sonicpanther101@users.noreply.github.com>
2025-02-11 23:00:40 +1300

    Hardware config

--- initialise.sh
+++ initialise.sh
@@ -1,9 +1,11 @@
 # install nixos and select no display manager when installing
 # nmcli device wifi connect wifi_name password wifi_password
-# nix-shell -p git
+# nix-shell -p git nh
 # git clone https://github.com/sonicpanther101/nixos-config
 # cd nixos-config
 # ./initialise.sh
 
 mkdir -p ~/Music
 mkdir -p ~/Pictures/wallpapers
+
+cp /etc/nixos/hardware-configuration.nix hosts/${HOST}/hardware-configuration.nix
