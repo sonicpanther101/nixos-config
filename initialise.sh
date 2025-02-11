2888f105c1372b4e2e6ca1cb869039b7284bdb2f
Astro <116870775+sonicpanther101@users.noreply.github.com>
2025-02-11 22:53:53 +1300

    Create initialise.sh

---
+++ initialise.sh
@@ -0,0 +1,7 @@
+# install nixos and select no display manager when installing
+# nmcli device wifi connect wifi_name password wifi_password
+# nix-shell -p git
+# git clone https://github.com/sonicpanther101/nixos-config
+# cd nixos-config
+# ./initialise.sh
+
