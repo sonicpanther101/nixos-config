d8512ad35f9e951bea41efb6a3d077386bfe599a
sonicpanther101 <sonicpanther101@gmail.com>
2025-11-16 19:28:30 +1300

    added audio configuration

---
+++ modules/core/audio.nix
@@ -0,0 +1,10 @@
+{ ... } :  {
+  services.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # lowLatency.enable = true;
+  };
+}
