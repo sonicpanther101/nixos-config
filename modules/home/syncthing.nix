5db4f94518686528f990cb0e53df4f0255c39ec6
sonicpanther101 <sonicpanther101@gmail.com>
2025-11-24 16:09:53 +1300

    added all current folders to syncthing

--- modules/home/syncthing.nix
+++ modules/home/syncthing.nix
@@ -5,7 +5,7 @@
       devices = {
         "Phone" = { id = "U3WDNWK-4BIUXM4-OS5SUHL-EZEFTWT-22NIYE6-QEZ6YS5-5WNR7QW-DYRUKAH"; };
       } // (if (host == "desktop") then {
-          "laptop" = { id = ""; };
+          "laptop" = { id = "K4TFO23-BFIJIKI-YCZVGVJ-HLHAEY7-6QJ2WHP-3QN6WJ3-DX74NUX-JNIVLAP"; };
         } else {
           "desktop" = { id = "7LMWSE7-SIOJROW-CSVA53V-RM77RLA-GBU2T6H-GL6SYLW-UC32RBB-M3455QQ"; };
         });
@@ -13,6 +13,21 @@
         "School" = {
           id = "r7hv7-6lau7";
           path = if (host == "desktop") then "/home/${username}/driveBig/School/" else "/home/${username}/School/";
+          devices = [ "Phone" ] ++ (if (host == "desktop") then [ "laptop" ] else [ "desktop" ]);
+        };
+        "Guitar" = {
+          id = "zevcu-jzwwx";
+          path = if (host == "desktop") then "/home/${username}/driveBig/Guitar/" else "/home/${username}/Guitar/";
+          devices = [ "Phone" ] ++ (if (host == "desktop") then [ "laptop" ] else [ "desktop" ]);
+        };
+        "Music" = {
+          id = "csoaz-zgdyy";
+          path = if (host == "desktop") then "/home/${username}/driveBig/Music/" else "/home/${username}/Music/";
+          devices = [ "Phone" ] ++ (if (host == "desktop") then [ "laptop" ] else [ "desktop" ]);
+        };
+        "Research Papers" = {
+          id = "p2539-fht3j";
+          path = if (host == "desktop") then "/home/${username}/driveBig/Research Papers/" else "/home/${username}/Research Papers/";
           devices = [ "Phone" ] ++ (if (host == "desktop") then [ "laptop" ] else [ "desktop" ]);
         };
       };
