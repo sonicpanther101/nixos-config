e18c304eb2a29f777fe15ac495ff13a2daf39f1e
sonicpanther101 <sonicpanther101@gmail.com>
2025-11-26 20:35:23 +1300

    Enabled hyprland autostart through xdg. Rebuilt desktop: Generation 179

--- modules/home/hyprland/hyprland.nix
+++ modules/home/hyprland/hyprland.nix
@@ -1,5 +1,7 @@
 { inputs, pkgs-stable, ... } : {
 
+  systemd.user.targets.hyprland-session.Unit.Wants = [ "xdg-desktop-autostart.target" ];
+
   wayland.windowManager.hyprland  = {
     enable = true;
 
