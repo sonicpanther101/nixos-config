056f74557f4c44e484bcce3702db9bdd63d88705
sonicpanther101 <sonicpanther101@gmail.com>
2025-11-26 18:39:25 +1300

    Fix hyprlock. Rebuilt desktop: Generation 171

--- modules/home/scripts/my-sleep.sh
+++ modules/home/scripts/my-sleep.sh
@@ -2,7 +2,12 @@ if ps aux | grep openrgb | grep -v S+ > /dev/null; then # Check if openrgb is ru
     sudo pkill openrgb                                  # Close existing openrgb                   
 fi                                                     
 openrgb --mode direct --color 000000                    # Turn all RGB off, without opening openrgb
-playerctl pause                                         # Pause all media                          
-systemctl suspend                                       # Put to sleep                             
-hyprlock                                                # Display lockscreen on wakeup             
+
+playerctl pause                                         # Pause all media   
+
+hyprlock &                                              # Start lockscreen in background
+sleep 0.5                                               # Give hyprlock time to initialize  
+
+systemctl suspend                                       # Put to sleep           
+                             
 openrgb --mode direct --color 000000                    # Turn all RGB off if started on boot
