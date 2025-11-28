afcb6203cd512861f3ba2046980ce360bf84a003
sonicpanther101 <sonicpanther101@gmail.com>
2025-11-28 16:43:12 +1300

    Set hypridle settings. Rebuilt desktop: Generation 209

--- modules/home/hyprland/hypridle.nix
+++ modules/home/hyprland/hypridle.nix
@@ -1,3 +1,31 @@
 { ... } : {
-  services.hypridle.enable = true;
+  services.hypridle = {
+    enable = true;
+
+    settings = {
+      general = {
+        # lock_cmd = "hyprlock";
+
+        # before_sleep_cmd = "hyprlock";
+        # after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+
+      listener = [
+        # {
+        #   timeout = 600;
+        #   on-timeout = "hyprlock";
+        # }
+        {
+          timeout = 60;
+          on-timeout = "hyprctl dispatch dpms off";
+
+          on-resume = "hyprctl dispatch dpms on";
+        }
+        # {
+        #   timeout = 1800;
+        #   on-timeout = "systemctl suspend";
+        # }
+      ];
+    };
+  };
 }
