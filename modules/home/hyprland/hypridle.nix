c83dcea101143bedbbd83971a7e558d48c1d8701
sonicpanther101 <sonicpanther101@gmail.com>
2025-11-29 19:00:53 +1300

    Added lock after 10mins and up to date with old config in core folder. Rebuilt desktop: Generation 226

--- modules/home/hyprland/hypridle.nix
+++ modules/home/hyprland/hypridle.nix
@@ -11,20 +11,20 @@
       };
 
       listener = [
-        # {
-        #   timeout = 600;
-        #   on-timeout = "hyprlock";
-        # }
         {
           timeout = 60;
           on-timeout = "hyprctl dispatch dpms off";
 
           on-resume = "hyprctl dispatch dpms on";
         }
-        # {
-        #   timeout = 1800;
-        #   on-timeout = "systemctl suspend";
-        # }
+        {
+          timeout = 600;
+          on-timeout = "pidof hyprlock || hyprlock";
+        }
+        {
+          timeout = 1800;
+          on-timeout = "my-sleep";
+        }
       ];
     };
   };
