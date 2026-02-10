59908790da7b83af62aeac30a0663cf27807eda0
Astro <116870775+sonicpanther101@users.noreply.github.com>
2026-02-11 01:03:52 +1300

    Updated network.nix to include my nextdns config to block ads system wide

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -1,11 +1,16 @@
 { host, ... } : {
-  networking.hostName = host; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking = {
+    hostName = host; # Define your hostname.
+    wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+    # Enable networking
+    networkmanager.enable = true;
+
+    # Set nextdns as network dns for all adblocking
+    nameservers = [ "176a88.dns.nextdns.io" ];
+  };
 }
