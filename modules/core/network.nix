9032e7c0555e4cbcc5c83c373617d9d2daacf9c3
sonicpanther101 <sonicpanther101@gmail.com>
2026-02-11 23:08:55 +1300

    Fix dns. Rebuilt desktop: Generation 306

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -1,15 +1,40 @@
 { host, ... } : {
   networking = {
     hostName = host; # Define your hostname.
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
     # Configure network proxy if necessary
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
     # Enable networking
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      dns = "none";  # Let dnscrypt-proxy handle DNS
+    };
+  };
 
-    # Set nextdns as network dns for all adblocking
-    nameservers = [ "176a88.dns.nextdns.io" ];
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      ipv6_servers = true;
+      require_dnssec = true;
+      
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+      
+      server_names = ["nextdns-176a88"];
+      
+      static.nextdns-176a88.stamp = "sdns://AgEAAAAAAAAAAAAQZG5zLm5leHRkbnMuaW8HL2RvaC8xNzZhODg";
+    };
+  };
+
+  systemd.services.dnscrypt-proxy2.serviceConfig = {
+    StateDirectory = "dnscrypt-proxy2";
   };
 }
