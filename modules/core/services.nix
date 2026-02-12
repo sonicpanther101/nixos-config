14c4a855e700687385f060cf8658ebd335162f41
sonicpanther101 <sonicpanther101@gmail.com>
2026-02-12 17:27:31 +1300

    Fixed waking from sleep. Rebuilt desktop: Generation 310

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -85,6 +85,10 @@
       ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="00c5", ATTR{power/wakeup}="disabled"
       ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0361", ATTR{power/wakeup}="disabled"
       SUBSYSTEMS=="usb|hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0361", TAG+="uaccess", TAG+="Keychron V6"
+      
+      # Disable network adapter wake-on-LAN/WiFi to prevent immediate wake from suspend
+      ACTION=="add", SUBSYSTEM=="net", KERNEL=="eth*|enp*", RUN+="${pkgs-stable.ethtool}/bin/ethtool -s $name wol d"
+      ACTION=="add", SUBSYSTEM=="net", KERNEL=="wl*", ATTR{device/power/wakeup}="disabled"
     '';
 
     # OpenRGB
