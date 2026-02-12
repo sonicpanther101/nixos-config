c4cae6c1118387d09350cb0f6e1caf0dd794d06b
sonicpanther101 <sonicpanther101@gmail.com>
2026-02-12 18:39:21 +1300

    Made vivaldi default browser. Rebuilt desktop: Generation 314

--- modules/home/defaultapps.nix
+++ modules/home/defaultapps.nix
@@ -1,3 +1,5 @@
+{ lib, pkgs-stable, ... }:
+
 let
   browser = "vivaldi-stable.desktop";
   editor = "codium.desktop";
@@ -14,8 +16,15 @@ in {
 
         "x-scheme-handler/http" = browser;
         "x-scheme-handler/https" = browser;
-        "application/xhtml+xml" = browser;
+        "x-scheme-handler/ftp" = browser;
+        "x-scheme-handler/chrome" = browser;
         "text/html" = browser;
+        "application/x-extension-htm" = browser;
+        "application/x-extension-html" = browser;
+        "application/x-extension-shtml" = browser;
+        "application/xhtml+xml" = browser;
+        "application/x-extension-xhtml" = browser;
+        "application/x-extension-xht" = browser;
 
         "x-scheme-handler/magnet" = torrent;
         "application/x-bittorrent" = torrent;
@@ -47,51 +56,6 @@ in {
         "image/vnd.wap.wbmp" = image-viewer;
         "image/x-icns" = image-viewer;
       };
-      associations = {
-        added = {
-          "inode/directory" = file-manager;
-
-          "x-scheme-handler/http" = browser;
-          "x-scheme-handler/https" = browser;
-          "application/xhtml+xml" = browser;
-          "text/html" = browser;
-
-          "x-scheme-handler/magnet" = torrent;
-          "application/x-bittorrent" = torrent;
-          "application/pdf" = browser;
-          "application/x-shellscript" = editor;
-
-          "image/jpeg" = image-editor;
-          "image/bmp" = image-viewer;
-          "image/gif" = image-viewer;
-          "image/jpg" = image-viewer;
-          "image/pjpeg" = image-viewer;
-          "image/png" = image-editor;
-          "image/tiff" = image-viewer;
-          "image/webp" = image-editor;
-          "image/x-bmp" = image-viewer;
-          "image/x-gray" = image-viewer;
-          "image/x-icb" = image-viewer;
-          "image/x-ico" = image-editor;
-          "image/x-png" = image-editor;
-          "image/x-portable-anymap" = image-viewer;
-          "image/x-portable-bitmap" = image-viewer;
-          "image/x-portable-graymap" = image-viewer;
-          "image/x-portable-pixmap" = image-viewer;
-          "image/x-xbitmap" = image-viewer;
-          "image/x-xpixmap" = image-viewer;
-          "image/x-pcx" = image-viewer;
-          "image/svg+xml" = image-viewer;
-          "image/svg+xml-compressed" = image-viewer;
-          "image/vnd.wap.wbmp" = image-viewer;
-          "image/x-icns" = image-viewer;
-        };
-      };
     };
-
-    # Force the change to override existing defaults
-    configFile."mimeapps.list".force = true;
-
   };
-
 }
