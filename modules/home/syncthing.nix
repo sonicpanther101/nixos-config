4df3a400c1ccaf3f13de167b361baa1d56ca5e75
sonicpanther101 <sonicpanther101@gmail.com>
2026-03-02 08:31:02 +1300

    Removed phone from syncthing. Rebuilt desktop: Generation 363

--- modules/home/syncthing.nix
+++ modules/home/syncthing.nix
@@ -13,17 +13,14 @@
       };
 
       devices = {
-        "Phone" = { 
-          id = "U3WDNWK-4BIUXM4-OS5SUHL-EZEFTWT-22NIYE6-QEZ6YS5-5WNR7QW-DYRUKAH";
-          # If your phone has a static IP on the uni wifi, put it here:
-          # addresses = [ "tcp://10.x.x.x:22000" ]; 
-        };
+        # "Phone" = { id = "U3WDNWK-4BIUXM4-OS5SUHL-EZEFTWT-22NIYE6-QEZ6YS5-5WNR7QW-DYRUKAH"; };
         "laptop" = { id = "K4TFO23-BFIJIKI-YCZVGVJ-HLHAEY7-6QJ2WHP-3QN6WJ3-DX74NUX-JNIVLAP"; };
         "desktop" = { id = "7LMWSE7-SIOJROW-CSVA53V-RM77RLA-GBU2T6H-GL6SYLW-UC32RBB-M3455QQ"; };
       };
       
       folders = let 
-        sharedDevices = [ "Phone" ] ++ (if host == "desktop" then [ "laptop" ] else [ "desktop" ]);
+        sharedDevices = (if host == "desktop" then [ "laptop" ] else [ "desktop" ]);
+        # sharedDevices = [ "Phone" ] ++ (if host == "desktop" then [ "laptop" ] else [ "desktop" ]);
         basePath = if host == "desktop" then "/home/${username}/driveBig" else "/home/${username}";
       in {
         "School" = { id = "r7hv7-6lau7"; path = "${basePath}/School/"; devices = sharedDevices; };
