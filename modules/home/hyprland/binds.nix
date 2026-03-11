213474a0b98cc66e98111563350e272d8053a0e5
sonicpanther101 <sonicpanther101@gmail.com>
2026-03-11 21:36:56 +1300

    Added keybinds for cycling workspaces and gestures. Rebuilt desktop: Generation 376

--- modules/home/hyprland/binds.nix
+++ modules/home/hyprland/binds.nix
@@ -100,8 +100,16 @@
       "SUPER SHIFT, 9, exec, hyprsome move 9"
       "SUPER SHIFT, 0, exec, hyprsome move 10"
       
-      "SUPER, mouse_down, exec, hyprsome workspace `hyprctl monitors -j | jq '.[] | select(.focused) | ((.activeWorkspace.id + 8) % 10) + 1'`"
       "SUPER, mouse_up, exec, hyprsome workspace `hyprctl monitors -j | jq '.[] | select(.focused) | (.activeWorkspace.id%10)+1'`"
+      "SUPER, mouse_down, exec, hyprsome workspace `hyprctl monitors -j | jq '.[] | select(.focused) | ((.activeWorkspace.id + 8) % 10) + 1'`"
+
+      "SUPER, Tab, exec, hyprsome workspace `hyprctl monitors -j | jq '.[] | select(.focused) | (.activeWorkspace.id%10)+1'`"
+      "SUPER SHIFT, Tab, exec, hyprsome workspace `hyprctl monitors -j | jq '.[] | select(.focused) | ((.activeWorkspace.id + 8) % 10) + 1'`"
+    ];
+
+    gestures = [
+      "3, left, exec, hyprsome workspace `hyprctl monitors -j | jq '.[] | select(.focused) | (.activeWorkspace.id%10)+1'`"
+      "3, right, exec, hyprsome workspace `hyprctl monitors -j | jq '.[] | select(.focused) | ((.activeWorkspace.id + 8) % 10) + 1'`"
     ];
 
     # keybinds that work even on lockscreen
