346657dab325aae9b1631f4c6308b2e428536dcb
sonicpanther101 <sonicpanther101@gmail.com>
2026-03-19 23:38:52 +1300

    Changed monitor scaling. Rebuilt desktop: Generation 389

--- modules/home/hyprland/settings.nix
+++ modules/home/hyprland/settings.nix
@@ -135,8 +135,8 @@
       ] else [];
 
       monitor = if (host == "desktop") then [
-        "DP-1,2560x1440@170,0x0,1.3333333"
-        "HDMI-A-1,1920x1080@100,1920x0,1"
+        "DP-1,2560x1440@170,0x0,1.3333"
+        "HDMI-A-1,1920x1080@100,1925x0,1"
       ] else [",preferred,auto,1.9"];
 
       xwayland.force_zero_scaling = true;
