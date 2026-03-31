11fe02caa2993352a5f9741370e387b1ff29dd0c
sonicpanther101 <sonicpanther101@gmail.com>
2026-04-01 00:06:00 +1300

    Focus correct monitor when returning from other monitor. Rebuilt desktop: Generation 401

--- modules/home/scripts/focus-move.sh
+++ modules/home/scripts/focus-move.sh
@@ -4,10 +4,19 @@ before_x=$(hyprctl activewindow -j | jq -r '.at[0]')
  
 hyprctl dispatch movefocus "$DIR" > /dev/null 2>&1
  
-after=$(hyprctl activewindow -j)
-after_x=$(echo "$after" | jq -r '.at[0]')
+after_x=$(hyprctl activewindow -j | jq -r '.at[0]')
  
 case "$DIR" in
-    l) [ "$after_x" -ge "$before_x" ] && hyprctl dispatch focusmonitor 1 ;;
-    r) [ "$after_x" -le "$before_x" ] && hyprctl dispatch focusmonitor 0 ;;
+    l)
+        [ "$after_x" -ge "$before_x" ] && 
+            active_workspace=$(hyprctl monitors -j | jq -r '.[] | select(.id == '1') | .activeWorkspace.id')
+            addr=$(hyprctl clients -j | jq -r '[.[] | select(.monitor == '1' and .workspace.id == '$active_workspace')] | max_by(.at[0]) | .address')
+            hyprctl dispatch focuswindow "address:$addr"
+        ;;
+    r)
+        [ "$after_x" -le "$before_x" ] && 
+            active_workspace=$(hyprctl monitors -j | jq -r '.[] | select(.id == '0') | .activeWorkspace.id')
+            addr=$(hyprctl clients -j | jq -r '[.[] | select(.monitor == '0' and .workspace.id == '$active_workspace')] | min_by(.at[0]) | .address')
+            hyprctl dispatch focuswindow "address:$addr"
+        ;;
 esac
