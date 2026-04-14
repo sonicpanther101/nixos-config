d04486890e5bedaedbef348ff933ac8b4a4bd6fd
sonicpanther101 <sonicpanther101@gmail.com>
2026-04-14 20:07:42 +1200

    Enabled sleep on laptop-2. Rebuilt laptop-2: Generation 15

--- hosts/laptop-2/hardware-configuration.nix
+++ hosts/laptop-2/hardware-configuration.nix
@@ -25,6 +25,10 @@
     size = 16 * 1024; # 16GB
   }];
 
+  boot.kernelParams = [ "mem_sleep_default=deep" "resume_offset=82728960" ];
+
+  boot.resumeDevice = "/dev/disk/by-uuid/69bad9c2-e9e7-4a44-bfe2-e1a3026a4a20";
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
