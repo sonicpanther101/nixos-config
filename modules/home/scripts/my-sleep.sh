4c565c608770189203e2c3724283460fd4151f6c
sonicpanther101 <sonicpanther101@gmail.com>
2026-04-15 10:55:51 +1200

    Fixed sleep on laptop-2. Rebuilt laptop-2: Generation 23

--- modules/home/scripts/my-sleep.sh
+++ modules/home/scripts/my-sleep.sh
@@ -3,9 +3,14 @@ host=$(hostname)
 if [[ $host == "desktop" ]]; then       
     openrgb --mode direct --color 000000 # Turn all RGB off, without opening openrgb
 fi                                      
+
 playerctl pause                          # Pause all media
-systemctl suspend                        # Put to sleep
-pidof swaylock || swaylock               # Display lockscreen on wakeup
+
+pidof swaylock || swaylock --daemonize   # Display lockscreen on wakeup
+
+sleep 0.5
+
+pkill hypridle && systemctl suspend      # Put to sleep
 
 if [[ $host == "desktop" ]]; then       
     openrgb --mode direct --color 000000 # Turn all RGB off if started on boot
@@ -15,5 +20,4 @@ imv ~/Pictures/system/Study\ times.png   # Open study schedule
 
 hyprctl dispatch dpms on                 # Turn display on
 
-cd ~/nixos-config                       
-git fetch
+cd ~/nixos-config && git fetch
