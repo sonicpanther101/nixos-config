4847d32e03cfed857d7adc6d245e51f2269e1fe6
sonicpanther101 <sonicpanther101@gmail.com>
2026-07-25 13:47:08 +1200

    Fixed backup script. Rebuilt laptop-2: Generation 101

--- modules/home/scripts/my-backup.sh
+++ modules/home/scripts/my-backup.sh
@@ -1,6 +1,6 @@
-if lsblk | grep Ventoy; then
-    cp /run/media/adam/Ventoy/Uni\ Notes /tmp/usb-backup
-    rm ~/Desktop/Uni\ Notes
+if lsblk | grep Ventoy >/dev/null 2>&1; then
+    cp -r /run/media/adam/Ventoy/Uni\ Notes /tmp/usb-backup
+    rm -rf ~/Desktop/Uni\ Notes
     mv /tmp/usb-backup ~/Desktop/Uni\ Notes
     echo "Last backed up $(date '+%l:%M%p')"
 fi
