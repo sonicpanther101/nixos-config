bb8b16ab7cb03e03e6e04e3431da708ec83d2e6d
sonicpanther101 <sonicpanther101@gmail.com>
2026-07-28 22:46:19 +1200

    Added service to fix wifi if it breaks. Rebuilt desktop: Generation 776

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -18,4 +18,20 @@
     # Point to local NextDNS proxy
     nameservers = [ "127.0.0.1" "::1" ];
   };
+
+  environment.etc."systemd/system-sleep/reload-rtw88.sh" = {
+    mode = "0755";
+    text = ''
+      #!/bin/sh
+      # Reload rtw88_8821ce after resume to work around wifi not
+      # reassociating after long suspends.
+      case "$1" in
+        post)
+          modprobe -r rtw88_8821ce
+          sleep 1
+          modprobe rtw88_8821ce
+          ;;
+      esac
+    '';
+  };
 }
