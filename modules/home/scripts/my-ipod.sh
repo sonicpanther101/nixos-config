9a31851d8c18e378f9a6184eb3c7e07e282ea275
sonicpanther101 <sonicpanther101@gmail.com>
2026-09-23 13:04:04 +1200

    Fix typo. Rebuilt desktop: Generation 858

--- modules/home/scripts/my-ipod.sh
+++ modules/home/scripts/my-ipod.sh
@@ -4,10 +4,10 @@ for f in *.flac; do ffmpeg -i "$f" -codec:a libmp3lame -qscale:a 2 "${f%.flac}.m
 rm *.flac
 
 # 2. Rename MP3 files according to ID3 metadata
-exiftool '-filename<${Band} - (${RecordingTime}) ${Album} - ${Track} ${Title}.%e' *.mp3
+exiftool -api "filter='tr(/)(-)'" '-filename<${Band} - (${RecordingTime}) ${Album} - ${Track} ${Title}.%e' *.mp3
 
-rm /run/media/adam/ADAM_S IPOD/iPod_Control/Music/F00/*
-cp *.mp3 /run/media/adam/ADAM_S IPOD/iPod_Control/Music/F00 
+rm /run/media/adam/ADAM_S\ IPOD/iPod_Control/Music/F00/*
+cp *.mp3 /run/media/adam/ADAM_S\ IPOD/iPod_Control/Music/F00 
 
 # 3. Update iPod database
 cd /run/media/adam/ADAM_S\ IPOD
