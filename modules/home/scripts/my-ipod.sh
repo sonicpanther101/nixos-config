e83674effaa72eee88dee589edba081f5f21fa8e
sonicpanther101 <sonicpanther101@gmail.com>
2026-09-23 13:11:59 +1200

    Sanitisation. Rebuilt desktop: Generation 859

--- modules/home/scripts/my-ipod.sh
+++ modules/home/scripts/my-ipod.sh
@@ -4,7 +4,7 @@ for f in *.flac; do ffmpeg -i "$f" -codec:a libmp3lame -qscale:a 2 "${f%.flac}.m
 rm *.flac
 
 # 2. Rename MP3 files according to ID3 metadata
-exiftool -api "filter='tr(/)(-)'" '-filename<${Band} - (${RecordingTime}) ${Album} - ${Track} ${Title}.%e' *.mp3
+exiftool -api 'Filter=s/\//-/g' '-filename<${Band} - (${RecordingTime}) ${Album} - ${Track} ${Title}.%e' *.mp3
 
 rm /run/media/adam/ADAM_S\ IPOD/iPod_Control/Music/F00/*
 cp *.mp3 /run/media/adam/ADAM_S\ IPOD/iPod_Control/Music/F00 
